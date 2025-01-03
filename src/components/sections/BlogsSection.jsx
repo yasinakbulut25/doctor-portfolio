0bb389aea23b9f55eed3c557bf1712a4a815a1bb
Yasin Akbulut <yasin@jotform.com>
2025-01-03 07:41:43 +0300

    update blog section link

--- src/components/sections/BlogsSection.jsx
+++ src/components/sections/BlogsSection.jsx
@@ -38,35 +38,33 @@ function BlogsSection() {
       <div className="grid gap-4 md:gap-8 md:grid-cols-2 lg:grid-cols-3 px-4">
         {blogPosts.map((post, index) => (
           <BlurFade key={index} delay={0.25 + index * 0.1} inView>
-            <div className="group flex flex-col gap-3 p-5 rounded-3xl bg-white border border-gray-100 shadow-2xl shadow-gray-600/10">
-              <div className="relative overflow-hidden rounded-xl">
-                <Image
-                  src={post.imageSrc}
-                  alt="art cover"
-                  loading="lazy"
-                  width={350}
-                  height={250}
-                  className="max-h-[350px] h-auto w-full object-cover object-center transition duration-500 group-hover:scale-105"
-                />
-              </div>
-              <Link href={post.link}>
+            <Link href="/blog">
+              <div className="group flex flex-col gap-3 p-5 rounded-3xl bg-white border border-gray-100 shadow-2xl shadow-gray-600/10">
+                <div className="relative overflow-hidden rounded-xl">
+                  <Image
+                    src={post.imageSrc}
+                    alt="art cover"
+                    loading="lazy"
+                    width={350}
+                    height={250}
+                    className="max-h-[350px] h-auto w-full object-cover object-center transition duration-500 group-hover:scale-105"
+                  />
+                </div>
                 <h3 className="text-lg font-bold text-black group-hover:text-purple-600 duration-300">
                   {post.title}
                 </h3>
-              </Link>
-              <div className="flex items-center justify-between gap-2">
-                <span className="text-slate-500 text-sm">{post.date}</span>
-                <Button
-                  className="bg-purple-600 text-white"
-                  as={Link}
-                  size="sm"
-                  href={post.link}
-                  endContent={<ChevronRight className="w-4 h-4" />}
-                >
-                  Yazıyı Oku
-                </Button>
+                <div className="flex items-center justify-between gap-2">
+                  <span className="text-slate-500 text-sm">{post.date}</span>
+                  <Button
+                    className="bg-purple-600 text-white"
+                    size="sm"
+                    endContent={<ChevronRight className="w-4 h-4" />}
+                  >
+                    Yazıyı Oku
+                  </Button>
+                </div>
               </div>
-            </div>
+            </Link>
           </BlurFade>
         ))}
       </div>
