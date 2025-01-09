9e90bf561ac4d50975bc0f0ba4626631d4fc23d1
Yasin Akbulut <yasin@jotform.com>
2025-01-09 08:11:51 +0300

    send email with nodemailer

--- src/components/sections/ContactForm.jsx
+++ src/components/sections/ContactForm.jsx
@@ -5,22 +5,48 @@ import InputField from "@/components/form/InputField";
 import TextAreaField from "@/components/form/TextAreaField";
 import SubmitButton from "@/components/form/SubmitButton";
 import BlurFade from "@/components/ui/blur-fade";
+import Chip from "../chip";
 
 function ContactForm({ map }) {
   const initialStates = {
+    name: "",
     email: "",
     title: "",
     message: "",
   };
   const [formData, setFormData] = useState(initialStates);
+  const [response, setResponse] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleChange = (input) => (e) => {
     setFormData({ ...formData, [input]: e.target.value });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(formData);
+    setIsLoading(true);
+
+    const response = await fetch("/api/send-email", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: formData.name,
+        email: formData.email,
+        title: formData.title,
+        message: formData.message,
+      }),
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      setFormData(initialStates);
+    }
+
+    setResponse(data);
+    setIsLoading(false);
   };
 
   return (
@@ -29,13 +55,24 @@ function ContactForm({ map }) {
         <form onSubmit={handleSubmit} encType="multipart/form-data">
           <div className="w-full flex flex-col gap-6 mx-auto">
             <InputField
+              type="text"
+              label="Ad Soyad"
+              placeholder="Adınızı ve soyadınızı yazınız.."
+              isRequired
+              maxLength={300}
+              onChange={handleChange("name")}
+              value={formData.name}
+              errorMessage="Lütfen geçerli bir değer ile doldurunuz!"
+            />
+            <InputField
               type="email"
               label="Mail Adresiniz"
               placeholder="Mail adresinizi yazınız.."
               isRequired
               maxLength={300}
               onChange={handleChange("email")}
-              errorMessage="Mail adresi boş bırakılamaz."
+              value={formData.email}
+              errorMessage="Lütfen geçerli bir değer ile doldurunuz!"
             />
             <InputField
               type="text"
@@ -44,17 +81,29 @@ function ContactForm({ map }) {
               isRequired
               maxLength={300}
               onChange={handleChange("title")}
-              errorMessage="Konu başlığı boş bırakılamaz."
+              value={formData.title}
+              errorMessage="Lütfen geçerli bir değer ile doldurunuz!"
             />
             <TextAreaField
               label="Mesajınız"
               isRequired
               placeholder="Mesajınızı yazınız.."
               onChange={handleChange("message")}
-              errorMessage="Mesaj boş bırakılamaz."
+              value={formData.message}
+              errorMessage="Lütfen geçerli bir değer ile doldurunuz!"
             />
-
-            <SubmitButton>Gönder</SubmitButton>
+            {response && (
+              <Chip
+                item={{
+                  type: response.error ? "error" : "success",
+                  value: response.error ? response.error : response.message,
+                }}
+                className="!w-full py-3"
+              />
+            )}
+            <SubmitButton isDisabled={isLoading} isLoading={isLoading}>
+              Gönder
+            </SubmitButton>
           </div>
         </form>
       </BlurFade>
